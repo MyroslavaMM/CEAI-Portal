c7dd73788226ec7f413e095a372c2bc08a71ce8f
myroslavamartyniuk <miroslava.martyniuk@bndigital.co>
2025-09-06 17:08:47 +0300

    updated landing page

--- ceaiportal.client/src/components/dashboardContent/index.tsx
+++ ceaiportal.client/src/components/dashboardContent/index.tsx
@@ -1,43 +1,65 @@
 import type { FC } from "react";
 import "./index.less";
-import { Row, Typography } from "antd";
+import { Col, Row, Typography } from "antd";
 import Header from "../header";
 const { Title, Paragraph } = Typography;
 import HomeIcon from "../../../public/icons/home.svg";
 import ToolsCard from "../toolsCard";
 import { agentsData, toolsData } from "../../data";
+import { DoubleRightOutlined } from "@ant-design/icons";
 
 const DashboardContent: FC = () => {
   const tools = agentsData.concat(toolsData);
   return (
     <Row className={"dashboard-wrapper"}>
       <Header name={"Dashboard"} icon={HomeIcon} />
-
-      <Row className={"dashboard-header-wrapper"}>
-        <Title className={"dashboard-title"} level={1}>
-          Welcome to the Catholic Extension AI Portal
-        </Title>
-        <Paragraph className={"dashboard-subtitle"}>
-          Discover easy-to-use AI tools designed to support your work and our
-          mission.
-        </Paragraph>
-      </Row>
-      <Row>
-        <Paragraph>
-          This portal gives you quick access to Catholic Extension’s AI tools.
-          Each tool is designed to help you find answers, work more efficiently,
-          and explore new ways to support our mission.
-        </Paragraph>
-      </Row>
-      <Row gutter={16} className={"card-tools-wrapper"}>
-        {tools.map(({ name, icon, id, type }) => (
-          <ToolsCard
-            name={name}
-            description="Ask questions and get instant answers based on CE magazine archive."
-            icon={icon}
-            link={type === "agents" ? `/agents/${id}` : `/tools/${id}`}
+      <Row className={"dashboard-content-wrapper"}>
+        <Col span={10}>
+          <Row className={"dashboard-header-wrapper"}>
+            <Title className={"dashboard-title"} level={1}>
+              Welcome to the Catholic Extension AI Portal
+            </Title>
+            <Paragraph className={"dashboard-subtitle"}>
+              Discover easy-to-use AI tools designed to support
+              <br /> your work and our mission.
+            </Paragraph>
+            <Paragraph className={"dashboard-about-section"}>
+              This portal gives you quick access to Catholic Extension’s AI
+              tools.
+              <br />
+              Each tool is designed to help you find answers, work more
+              efficiently, and explore new ways to support our mission.
+            </Paragraph>
+          </Row>
+        </Col>
+        <Col span={11} className={"card-tools-col"}>
+          <DoubleRightOutlined
+            className={"tools-carousel-icon"}
+            style={{
+              transform: "rotate(270deg)",
+              marginBottom: 15,
+              opacity: 0.2,
+            }}
           />
-        ))}
+          <Row gutter={16} className={"card-tools-wrapper"}>
+            {tools.map(({ name, icon, id, type }) => (
+              <ToolsCard
+                name={name}
+                description="Ask questions and get instant answers based on CE magazine archive."
+                icon={icon}
+                link={type === "agents" ? `/agents/${id}` : `/tools/${id}`}
+              />
+            ))}
+          </Row>
+          <DoubleRightOutlined
+            className={"tools-carousel-icon"}
+            style={{
+              transform: "rotate(90deg)",
+              marginTop: 15,
+              opacity: 0.2,
+            }}
+          />
+        </Col>
       </Row>
     </Row>
   );
